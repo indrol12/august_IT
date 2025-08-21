a861438b2a44af6e1080936a762f6d5267b454d1
indrol12 <indrolofficial@gmail.com>
2025-08-21 16:47:39 +0530

    committed

---
+++ votingeligibility.java
@@ -0,0 +1,14 @@
+public class votingeligibility{
+
+public static void main(String []args)
+{
+int age=67;
+if(age>=18)
+{
+System.out.println("eligible for voting");
+}
+else{
+System.out.println("not eligible for voting");
+}
+}
+}
